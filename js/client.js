714b218cb6bfd1d017bda7821ccc1d8ca18920ff
Dave <info@new-fantasy.eu>
2020-12-05 19:24:59 +0100

    add emojis to the chat | implement usernames | some style changes

--- js/client.js
+++ js/client.js
@@ -3,11 +3,24 @@
 var server = "http://127.0.0.1:3000/"
 var io = io(server)
 
+const username = prompt("Pick username")
+
+const emojis = document.getElementsByClassName("icon")
+
+for(let i = 0; i < emojis.length; i++){
+    const icon = emojis[i]
+    icon.addEventListener("click", function(e){
+        console.log("target.id", e.target.id)
+        document.getElementById("m").value += e.target.id.toUpperCase() + " "
+    })
+}
+
+
 function sendMessage(){
     const message = document.getElementById("m").value
 
     //sende nachricht vom client
-    io.emit("new_massage", message)
+    io.emit("new_massage", message, username)
     // setze die form zurück auf einen leeren string
     document.getElementById("m").value = ""
     // prevent to submmitting the form
@@ -16,11 +29,15 @@ function sendMessage(){
 
 
 // Hört vom server auf das event new_massage
-io.on("new_massage", function(data){
-    console.log("server says: ", data)
+io.on("new_massage", function(data, username){
+    console.log("server says: ", data, username)
     // Zeige message an
     const li = document.createElement("li")
-    li.innerText = data
+    const div = document.createElement("div")
+    div.innerHTML = `<span style="color: green">${username}</span>: ${data}`
+    div.innerHTML += '<button style="color:red">delete</button>'
+    li.append(div)
+    console.log(li.innerHTML)
     const messages = document.getElementById("messages")
     messages.append(li)
 })
--- server.js
+++ server.js
@@ -4,10 +4,8 @@ console.log("Chat Server Javascript")
 
 // use express
 const express = require("express")
-
 // Instanz von express erstellen
 const app = express()
-
 // benutze http mit einer Instanz von express
 const http = require("http").createServer(app)
 
@@ -26,11 +24,41 @@ io.on("connection", function(socket){
     console.log("User connected", socket.id)
 
     //server hört zu wenn new_massage reinkommt
-    socket.on("new_massage", function(data){
-      console.log("User Sagt: ", data)
+    socket.on("new_massage", function(data, username){
+      
+      let text_array = data.split(" ")
+      for (let i = 0; i < text_array.length; i++){
+        if(text_array[i] === "EMOJI1"){
+          text_array[i] = '<img src="/img/01.png" alt="">'
+        }
+        if(text_array[i] === "EMOJI2"){
+          text_array[i] = '<img src="/img/2.png" alt="">'
+        }
+        if(text_array[i] === "EMOJI3"){
+          text_array[i] = '<img src="/img/3.png" alt="">'
+        }
+        if(text_array[i] === "EMOJI4"){
+          text_array[i] = '<img src="/img/4.png" alt="">'
+        }
+        if(text_array[i] === "EMOJI5"){
+          text_array[i] = '<img src="/img/5.png" alt="">'
+        }
+        if(text_array[i] === "EMOJI6"){
+          text_array[i] = '<img src="/img/6.png" alt="">'
+        }
+        if(text_array[i] === "EMOJI7"){
+          text_array[i] = '<img src="/img/7.png" alt="">'
+        }
+        if(text_array[i] === "EMOJI8"){
+          text_array[i] = '<img src="/img/8.png" alt="">'
+        }
+
+      }
+      var newTxt = text_array.join(' ')
+      console.log(newTxt)
 
       // server sendet an alle verbundenen clients
-      io.emit("new_massage", data)
+      io.emit("new_massage", newTxt, username)
     })
 })
 
